ba69c0dc910c61b0959e75a9b081adf578262001
Amit pandey <amitpandeye120@gmail.com>
2024-11-15 10:33:49 +0530

    Amit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      count:0
+    }
+  }
+  shouldComponentUpdate(){
+    console.warn("shouldComponentUpdate",this.state.count)
+    // return false 
+    if(this.state.count>=5 && this.state.count<=10){
+      return true 
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>Hello {this.state.count}</h1>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>Update Name</button>
+      </div>
+    )
+  }
 }
 
 export default App;
